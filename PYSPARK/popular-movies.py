6c2f4fceeaa9108dc5398283b44d75d7797605cd
Shakeel Ansari <ShakeelAnsari63@gmail.com>
2019-11-13 14:08:49 +0530

    Popular Movies with Broadcast Variable

--- PYSPARK/popular-movies.py
+++ PYSPARK/popular-movies.py
@@ -3,8 +3,8 @@ from pyspark import SparkConf, SparkContext
 conf = SparkConf().setMaster("local").setAppName("PopularMovies")
 sc = SparkContext(conf = conf)
 
-lines = sc.textFile("file:///SparkCourse/ml-100k/u.data")
-movies = lines.map(lambda x: (int(x.split()[1]), 1))
+lines = sc.textFile("/home/freak/Project/datasets/ml-10M/ratings.dat")
+movies = lines.map(lambda x: (int(x.split('::')[1]), 1))
 movieCounts = movies.reduceByKey(lambda x, y: x + y)
 
 flipped = movieCounts.map( lambda xy: (xy[1],xy[0]) )
