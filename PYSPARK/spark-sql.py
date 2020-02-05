da3ceca086e5742afa0b540560e6105da5e92600
Shakeel Ansari <ShakeelAnsari63@gmail.com>
2020-02-05 19:20:53 +0530

    Pyspark SQL and Dataframes

--- PYSPARK/spark-sql.py
+++ PYSPARK/spark-sql.py
@@ -20,6 +20,9 @@ schemaPeople.createOrReplaceTempView("people")
 # SQL can be run over DataFrames that have been registered as a table.
 teenagers = spark.sql("SELECT * FROM people WHERE age >= 13 AND age <= 19")
 
+# Show Teenager Table
+teenagers.show()
+
 # The results of SQL queries are RDDs and support all the normal RDD operations.
 for teen in teenagers.collect():
   print(teen)
--- PYTHON-TKINTER/03_button.py
+++ PYTHON-TKINTER/03_button.py
@@ -1,8 +1,8 @@
 import tkinter as tk
 
-def click_action():
-    tk.Label(root_widget, text = "You clicked it and something happened !!!!!")\
-        .grid(row = 1, column = 0, columnspan = 2, sticky = tk.W)
+def click_action(widget, row, column):
+    tk.Label(widget, text = "You clicked it and something happened !!!!!")\
+        .grid(row = row, column = column, columnspan = 2, sticky = tk.W)
 
 # In tkinter everything is widget and first you should create root widget
 root_widget = tk.Tk()
@@ -15,7 +15,7 @@ root_widget = tk.Tk()
 button = tk.Button(root_widget, text = "Do Nothing", state = tk.DISABLED, padx = 50, pady = 50)
 button.grid(row = 0, column = 0)
 
-button = tk.Button(root_widget, text = "Do Something", padx = 50, pady = 50, command = click_action)
+button = tk.Button(root_widget, text = "Do Something", padx = 50, pady = 50, command = lambda: click_action(root_widget, 1, 0))
 button.grid(row = 0, column = 1)
 
 # To show the Widget, call mainLoop method
