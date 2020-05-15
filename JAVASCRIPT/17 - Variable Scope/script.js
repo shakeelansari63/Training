e63d304f8d83db925b45d2913421dbe68411e684
Shakeel Ansari <ShakeelAnsari63@gmail.com>
2020-05-16 02:14:44 +0530

    Add Error

--- JAVASCRIPT/17 - Variable Scope/script.js
+++ JAVASCRIPT/17 - Variable Scope/script.js
@@ -1,17 +1,17 @@
-function findBiggestFraction(a,b) {
+function findBiggestFraction(a, b) {
     console.log("Fraction a: ", firstFraction);
     console.log("Fraction b: ", secondFraction);
 
     var result; // Local Scope
 
-	a>b ? result = ["a",a] : result = ["b",b];
+    a > b ? result = ["a", a] : result = ["b", b];
     return result;
 }
 
-var firstFraction = 7/16; // Global Scope
-var secondFraction = 13/25; // Global Scope
+var firstFraction = 7 / 16; // Global Scope
+var secondFraction = 13 / 25; // Global Scope
 
-var fractionResult = findBiggestFraction(firstFraction,secondFraction);
+var fractionResult = findBiggestFraction(firstFraction, secondFraction);
 console.log("Fraction " + fractionResult[0] + " with a value of " + fractionResult[1] + " is the biggest.");
 
 // This gives error since result is not defied in global Scope
