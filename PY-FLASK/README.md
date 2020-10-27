73af16038fc3c5b1912d269996abdf258a4d570c
Shakeel Ansari <ShakeelAnsari63@gmail.com>
2020-10-28 01:21:10 +0530

    Python Flask

---
+++ PY-FLASK/README.md
@@ -0,0 +1,25 @@
+### Install Flask
+```
+pip install flask
+```
+
+### Run Flask App
+```
+# Set Script to run
+export FLASK_APP=main.py
+
+# Run Flask app
+flask run
+```
+
+### Start app in Debug Mode
+```
+# Set Script to run
+export FLASK_APP=main.py
+
+# Set Degug Mode
+export FLASK_DEBUG=1
+
+# Run Flask app
+flask run
+```
--- PY-FLASK/app.py
+++ PY-FLASK/app.py
@@ -7,20 +7,22 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+
 class ToDo(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    content = db.Column(db.String(200), nullable = False)
-    completed = db.Column(db.Integer, default = 0)
-    date_created = db.Column(db.DateTime, default = datetime.utcnow)
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    completed = db.Column(db.Integer, default=0)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return '<Task {}>'.format(self.id)
 
-@app.route('/', methods = ['POST','GET'])
+
+@app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
         task_content = request.form['content']
-        task = ToDo(content = task_content)
+        task = ToDo(content=task_content)
         try:
             db.session.add(task)
             db.session.commit()
@@ -29,7 +31,8 @@ def index():
             return 'Error occured'
     else:
         tasks = ToDo.query.order_by(ToDo.date_created).all()
-        return render_template('index.html', tasks = tasks)
+        return render_template('index.html', tasks=tasks)
+
 
 @app.route('/delete/<int:id>')
 def delete(id):
@@ -41,11 +44,12 @@ def delete(id):
     except:
         return 'Error Occured'
 
-@app.route('/update/<int:id>', methods = ['GET', 'POST'])
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
 def update(id):
     task_to_update = ToDo.query.get_or_404(id)
     if request.method == 'GET':
-        return render_template('update.html', task = task_to_update)
+        return render_template('update.html', task=task_to_update)
     else:
         task_to_update.content = request.form['content']
         try:
@@ -54,5 +58,6 @@ def update(id):
         except:
             return "Error Occured"
 
+
 if __name__ == "__main__":
-    app.run(host = '0.0.0.0', port = '8081')
+    app.run(host='0.0.0.0', port='8081')
