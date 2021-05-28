7fac02cb66f2e1240bb0db570f130865ef1be9ff
Shakeel Ansari <Shakeel.Ansari@nab.com.au>
2021-05-29 01:52:51 +0530

    Hide Add button on routes other than home

--- ANGULAR/src/app/components/header/header.component.ts
+++ ANGULAR/src/app/components/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UiService} from '../../services/ui.service'
 import { Subscription } from 'rxjs'
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-header',
@@ -13,7 +14,7 @@ export class HeaderComponent implements OnInit {
   newTaskActive: boolean;
   subscription: Subscription;
 
-  constructor(private uiService: UiService) {
+  constructor(private uiService: UiService, private router: Router) {
     // Connect subscriotion to Observable
     this.subscription = this.uiService.onToggleTask().subscribe((value) => {
       this.newTaskActive = value;
@@ -28,5 +29,11 @@ export class HeaderComponent implements OnInit {
 
     // Call UI Service toggle to toggle options
     this.uiService.toggleNewTask()
+  }
+
+  // return true is input route is matching the current route. 
+  // This will help us to hide button on any other route except index '/' route
+  routeMatch(route: string) {
+    return this.router.url === route;
   }
 }
