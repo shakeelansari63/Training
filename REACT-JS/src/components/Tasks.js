0873886d0cbf9ddeb569dca406c03cbc95a682f5
Shakeel Ansari <ShakeelAnsari63@gmail.com>
2021-05-31 19:56:26 +0530

    Use State

--- REACT-JS/src/components/Tasks.js
+++ REACT-JS/src/components/Tasks.js
@@ -1,33 +1,63 @@
+// If we need reactive UI, we should use state for changing data
+import { useState } from 'react'
 // Here is a Dummy task for us to work with
-const tasks = [
-    {
-        id: 1,
-        text: 'Go to School',
-        day: 'May 30 2021 12 PM',
-        reminder: true,
-    },
-    {
-        id: 2,
-        text: 'Search Breakfast',
-        day: 'May 31 2021 7 AM',
-        reminder: true,
-    },
-    {
-        id: 3,
-        text: 'Food Shopping',
-        day: 'June 1 2021 5 PM',
-        reminder: false,
-    },
+// const tasks = [
+//     {
+//         id: 1,
+//         text: 'Go to School',
+//         day: 'May 30 2021 12 PM',
+//         reminder: true,
+//     },
+//     {
+//         id: 2,
+//         text: 'Search Breakfast',
+//         day: 'May 31 2021 7 AM',
+//         reminder: true,
+//     },
+//     {
+//         id: 3,
+//         text: 'Food Shopping',
+//         day: 'June 1 2021 5 PM',
+//         reminder: false,
+//     },
 
-]
+// ]
 
 const Tasks = () => {
-    return(
+    // In order to use State, the tasks should become part of class
+    const [tasks, setTasks] = useState(
+        [
+            {
+                id: 1,
+                text: 'Go to School',
+                day: 'May 30 2021 12 PM',
+                reminder: true,
+            },
+            {
+                id: 2,
+                text: 'Search Breakfast',
+                day: 'May 31 2021 7 AM',
+                reminder: true,
+            },
+            {
+                id: 3,
+                text: 'Food Shopping',
+                day: 'June 1 2021 5 PM',
+                reminder: false,
+            },
+
+        ]
+    )
+
+    // Now we have setState function available and we can use this function to set new state of data
+    // like 
+    // settasks([...tasks], {new task object})
+    return (
         // if we want to return multiple objects without parent, we can 
         // put it in empty tag
         // It is mandatory to give unique key prop if returning list
         <>
-            { tasks.map( (task) => (<h4 key={task.id}>{task.text}</h4>))}
+            { tasks.map((task) => (<h4 key={task.id}>{task.text}</h4>))}
         </>
     );
 }
