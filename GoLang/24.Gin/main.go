18d0981e61857c1772d6de2116236fdc93502913
Shakeel Ansari <ShakeelAnsari63@gmail.com>
2021-05-31 19:49:52 +0530

    Go Module for GIN

---
+++ GoLang/24.Gin/main.go
@@ -0,0 +1,17 @@
+package main
+
+// Install Gin using 'go get -v github.com/gin-gonic/gin'
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
