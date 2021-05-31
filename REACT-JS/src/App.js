c5719f3664c5d793ed694f8d8670d2ef128ec6bf
Shakeel Ansari <ShakeelAnsari63@gmail.com>
2021-06-01 01:47:12 +0530

    Integrate with Json Server

--- REACT-JS/src/App.js
+++ REACT-JS/src/App.js
@@ -3,8 +3,8 @@ import Header from './components/Header'
 import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
 
-// Import useStates
-import { useState } from 'react'
+// Import useStates & useEffect to load something in beginning
+import { useState, useEffect } from 'react'
 
 // This is main app which is embedded in 
 function App() {
@@ -12,49 +12,86 @@ function App() {
   const [formView, setFormView] = useState(false)
 
   // We will maintain data in App which will be global Scope
-  const [tasks, setTasks] = useState(
-    [
-      {
-        id: 1,
-        text: 'Go to School',
-        day: 'May 30 2021 12 PM',
-        reminder: true,
-      },
-      {
-        id: 2,
-        text: 'Search Breakfast',
-        day: 'May 31 2021 7 AM',
-        reminder: true,
-      },
-      {
-        id: 3,
-        text: 'Food Shopping',
-        day: 'June 1 2021 5 PM',
-        reminder: false,
-      },
+  const [tasks, setTasks] = useState([])
 
-    ]
-  )
+  // Function to fetch Json data from API
+  const apiUrl = 'http://localhost:5000/tasks'
 
-  const deleteTask = (taskId) => {
-    console.log(taskId + ' Deleted !!')
+  const fetchTasks = async () => {
+    const resp = await fetch(apiUrl)
+    const tasks = await resp.json()
+
+    return tasks
+  }
+
+  // Method to fetch individual task from server
+  const fetchTask = async (id) => {
+    const getUrl = `${apiUrl}/${id}`
+    const resp = await fetch(getUrl)
+    const task = await resp.json()
+
+    return task
+  }
+
+  // Lets use useEffect to call API Url as soon as page laods
+  useEffect(() => {
+    // Define function for async get
+    const getTasks = async () => {
+      const tasks = await fetchTasks()
+      setTasks(tasks)
+    }
+
+    // Call Async function to get data and set State
+    getTasks();
+  }, [])
+
+  // Delete Task from UI and Backend
+  const deleteTask = async (taskId) => {
+    console.log(taskId + ' Deleted !!');
+
+    // Delete task from backend
+    const delUrl = `${apiUrl}/${taskId}`;
+    await fetch(delUrl, { method: 'DELETE' });
 
     // To Delete the task, we have to use SetTask state
-    const newTask = tasks.filter((task) => task.id !== taskId)
-    setTasks(newTask)
-  }
+    const newTask = tasks.filter((task) => task.id !== taskId);
+    setTasks(newTask);
+  };
 
-  const toggleReminder = (taskId) => {
+  // Update reminder in backend and UI
+  const toggleReminder = async (taskId) => {
     console.log(taskId + ' reminder updated !!')
 
+    // Update URl
+    const updUrl = `${apiUrl}/${taskId}`
+
+    // Get Task current data
+    const oldTask = await fetchTask(taskId);
+    // udpate Task reminder
+    const newTask = { ...oldTask, reminder: !oldTask.reminder }
+
+    console.log(newTask)
+
+    // Update the backend
+    const resp = await fetch(updUrl, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      }, body: JSON.stringify(newTask)
+    })
+
+    const data = await resp.json()
+
     // To update reminder, we have to create new task
-    const newTask = tasks.map((task) =>
-      task.id === taskId ? { ...task, reminder: !task.reminder } : task
+    const newTasks = tasks.map((task) =>
+      task.id === taskId ? data : task
     )
-    setTasks(newTask)
+
+    setTasks(newTasks)
   }
 
-  const createTask = (task) => {
+  // Add task in Backend and UI
+  const createTask = async (task) => {
     // generate an Id 
     // Map will return list of ids
     // Math.max calculate max from the list
@@ -64,8 +101,17 @@ function App() {
 
     console.log(newTask)
 
+    // Update backend
+    const resp = await fetch(apiUrl, {
+      method: 'POST', headers: {
+        'Content-Type': 'application/json'
+      }, body: JSON.stringify(newTask)
+    });
+
+    const data = await resp.json()
+
     // Adding task to state
-    const newTasks = [...tasks, newTask]
+    const newTasks = [...tasks, data]
     setTasks(newTasks)
   }
 
