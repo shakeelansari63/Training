501e2ef7abe9f6212904f3a8c3b655bd89dd422b
Shakeel Ansari <ShakeelAnsari63@gmail.com>
2021-10-29 13:32:43 +0530

    Created User Controller

---
+++ CSharp+Angular/API/Controllers/UsersController.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    // This is Users Controller which will server api/users endpoint
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        // Constructor for initializing Database Context
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // Http Get handler for api/users
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            return await _context.User.ToListAsync();
+        }
+
+        // HttpGet handler for api/users/{id}
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUserById(int id)
+        {
+            return await _context.User.FindAsync(id);
+        }
+    }
+}
---
+++ CSharp+Angular/API/Data/Migrations/20211029073758_AddSampleUsers.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Data.Migrations
+{
+    public partial class AddSampleUsers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            //Sql("Insert into User (Id, UserName) values (1, 'Bob');");
+            migrationBuilder.InsertData(
+                table: "User",
+                columns: new string[] { "Id", "UserName" },
+                values: new string[] { "1", "Bob" }
+            );
+
+            migrationBuilder.InsertData(
+                table: "User",
+                columns: new string[] { "Id", "UserName" },
+                values: new string[] { "2", "Tom" }
+            );
+
+            migrationBuilder.InsertData(
+                table: "User",
+                columns: new string[] { "Id", "UserName" },
+                values: new string[] { "3", "Jane" }
+            );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
