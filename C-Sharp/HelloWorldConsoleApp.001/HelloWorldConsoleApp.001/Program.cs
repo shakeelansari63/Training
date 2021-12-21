b7ba43efbd49e6cdb1cff9f8a23ebd41e619e651
Shakeel Ansari <ShakeelAnsari63@Gmail.com>
2021-12-21 18:22:22 +0530

    New C# Learning

---
+++ C-Sharp/HelloWorldConsoleApp.001/HelloWorldConsoleApp.001/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorldConsoleApp._001
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
