2a50c194d062d71bec885aaab31cdfbf24b26bd5
Shakeel Ansari <ShakeelAnsari63@gmail.com>
2022-02-21 09:47:33 +0530

    C# Methods

--- C-Sharp/C# Beginner/App04.Methods/Program.cs
+++ C-Sharp/C# Beginner/App04.Methods/Program.cs
@@ -6,7 +6,21 @@ namespace App04.Methods
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MethodWithoutParamAndReturn();
+
+            MethodWithParameters("Some string goes here");
+        }
+        
+        // We can only access static methods from otehr static metods.
+        // Since Main is static, this method also need to be static
+        public static void MethodWithoutParamAndReturn()
+        {
+            Console.WriteLine("Hello I am method without any parameter and return value");
+        }
+
+        public static void MethodWithParameters(string someParam)
+        {
+            Console.WriteLine("Hello this is your parameter - " + someParam);
         }
     }
 }
