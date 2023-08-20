39746a5d645c8d9f808268838b5579ba4ab5c6bc
Shakeel Ansari <Shakeel.Ansari@Gmail.com>
2023-08-20 19:21:11 +0530

    update app

--- FLUTTER/app01_hello_world_scaffold/test/widget_test.dart
+++ FLUTTER/app01_hello_world_scaffold/test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:app01_hello_world_scaffold/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(MyScaffoldApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
