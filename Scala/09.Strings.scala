1d154ad2082197c639ad83e03e15f52c1a9835cf
Shakeel Ansari <ShakeelAnsari63@Gmail.com>
2023-12-28 20:38:49 +0530

    Add String

--- Scala/09.Strings.scala
+++ Scala/09.Strings.scala
@@ -1,4 +1,5 @@
 import scala.util.matching._
+import java.time._
 
 // In Scala String as values in double quotes ""
 val a: String = "Hello"
@@ -30,3 +31,25 @@ println(regex1.findAllIn(data).toList)
 // Now same can be written with smart string without useless \\
 val regex2: Regex = """\s*\d?\d:\d\d\s*(AM|PM)""".r
 println(regex2.findAllIn(data).toList)
+
+// String Formatting
+// We can use .format method on Stiring for parameter replacement
+// example
+println("Hey there, this is %s".format("Test"))
+
+// And position of parameter can speficied as - %{position}:${format}
+println("Hey there... %3$s, %2$s, %1$s ... Go".format("One", "Two", "Three"))
+
+// And it also support Date Time replacement wth %t{format}
+println("Today is %1$te %1$tB of year %1$tY".format(LocalDate.now))
+
+// String Interpolation
+// Scala also has string using s or f string
+// s"" String as basic interpolation without lot of format options
+// Example
+val e: Int = 32
+println(s"Your age is ${e}")
+
+// f"" string is advanced and can be used for more formating. It combines s string and String Format
+// example - We can define a float precission format.
+println(f"Your age is ${e}%1.2f")
