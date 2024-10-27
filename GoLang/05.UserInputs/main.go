4a77f6c8b8c3bcc97b77c07676edcd649b725076
Shakeel Ansari63 <shakeelansari63@gmail.com>
2024-10-27 19:46:40 +0530

    user inp and str conv

--- GoLang/05.UserInputs/main.go
+++ GoLang/05.UserInputs/main.go
@@ -1,1 +1,33 @@
 package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+// We will use bufio builtin package to read user input from Standard Input
+
+func main() {
+	// To read input from Standard in we need buffer reader
+	reader := bufio.NewReader(os.Stdin)
+
+	// This reader will be used to read data from std in. Now lets print a message to tell user for input
+	fmt.Println("Enter a number: ")
+
+	// Reader object returns 2 objects.
+	// First - String entered by user in StdIn
+	// Second - Error if any error occur
+	// This is error handling method in Go. Instead of raising error, it returns error as object
+	// Which can be checked and action can be taken
+	// But if we want to ignore the error, we can use , _ syntax also called comma ok syntax.
+	// In this way, the second returned value is ignored
+	num, _ := reader.ReadString('\n')
+	fmt.Println("Your Number is : ", num)
+
+	// Lets see what is there in error object
+	fmt.Println("Enter another number: ")
+	x, err := reader.ReadString('\n')
+	fmt.Printf("Value: %s, Type: %T\n\n", x, x)
+	fmt.Printf("Value: %s, Type: %T", err, err)
+}
--- GoLang/06.DataConversion/main.go
+++ GoLang/06.DataConversion/main.go
@@ -1,1 +1,59 @@
 package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	// Lets say we have a string where some value exist in float
+	// While the data type is string, we cannot perform any numeric opration
+	// So we have to convert the data type of the variable to float
+	// We that using strconv package
+	n1s := "32"
+	n1, _ := strconv.ParseFloat(n1s, 32) // Parse into 32 bit float
+
+	fmt.Println("Added 1 to your number: ", n1+1)
+
+	// But if there is error in conversion, how would we know? It is captured through 2nd return object i.e. error
+	n2, err1 := strconv.ParseFloat("ab", 32)
+
+	// And we check error as follow
+	if err1 != nil {
+		fmt.Println("Error occured", err1)
+	} else {
+		fmt.Println("Added 1 to your number: ", n2+1)
+	}
+	// This will return error message
+
+	// So where to use this conversion?
+	// Best example is while reading from standart input
+	fmt.Println("Enter your number: ")
+	reader := bufio.NewReader(os.Stdin)
+	n3s, _ := reader.ReadString('\n')
+	n3, err2 := strconv.ParseFloat(n3s, 32)
+
+	// And we check error as follow
+	if err2 != nil {
+		fmt.Println("Error occured", err2)
+	} else {
+		fmt.Println("Added 1 to your number: ", n3+1)
+	}
+
+	// So what happened here? Why did we get error?
+	// This is because stdin returns new line character in outout value
+	// This need to be trimmed
+	fmt.Println("Lets try again\nEnter your number: ")
+	n4s, _ := reader.ReadString('\n')
+	n4, err3 := strconv.ParseFloat(strings.TrimSpace(n4s), 32)
+
+	// And we check error as follow
+	if err3 != nil {
+		fmt.Println("Error occured", err3)
+	} else {
+		fmt.Println("Added 1 to your number: ", n4+1)
+	}
+}
