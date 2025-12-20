d60223345bbc2b1bd5317c35625f6b526adbcd4a
Shakeel Ansari <ShakeelAnsari63@gmail.com>
2025-12-20 17:11:31 +0530

    Update Trainings

---
+++ JAVA/src/ed.java.p006.UserInput/Main.java
@@ -0,0 +1,13 @@
+package ed.java.p006.UserInput;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+        System.out.println("Hello, " + name + "!");
+    }
+}
