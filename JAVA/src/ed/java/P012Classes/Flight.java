e5834c2191eeb333d7183bfd69053f3c291862fd
Shakeel Ansari <ShakeelAnsari63@gmail.com>
2025-12-23 21:04:29 +0530

    classes

--- JAVA/src/ed/java/P012Classes/Flight.java
+++ JAVA/src/ed/java/P012Classes/Flight.java
@@ -3,13 +3,25 @@ package ed.java.P012Classes;
 class Flight {
 
     // There is internal state or members
-    int seat;
+    int seat = 100;
     int passangers;
 
-    // This is class constructor
-    Flight() {
-        this.seat = 50;
-        this.passangers = 0;
+    // This is a default class constructor
+    // It will be added automatically by Java if we don't have any other constructor
+    public Flight() {}
+
+    // We can have more constructors in this class
+    // This is a parameterized class constructor
+    public Flight(int seat) {
+        this.seat = seat;
+    }
+
+    // We can chain constructors using this keyword
+    public Flight(int seat, int passangers) {
+        // Call constructor with 1 parameter
+        this(seat);
+        // Set other values
+        this.passangers = passangers;
     }
 
     // These are some class methods
--- JAVA/src/ed/java/P012Classes/Main.java
+++ JAVA/src/ed/java/P012Classes/Main.java
@@ -5,25 +5,42 @@ public class Main {
     public static void main(String[] args) {
         // Create an instance of Flight
         // We don't need to import anything here are Fligt is in same package
-        Flight flight = new Flight();
+        // Default constructor will create flight with default 100 seats and 0 passangers
+        Flight flight1 = new Flight();
+
+        // We can create flight with custom number of seats
+        Flight flight2 = new Flight(200);
+
+        // Or with custom seats and passangers
+        Flight flight3 = new Flight(150, 50);
+
+        System.out.println("######### Flight - 1 #########");
 
         // Print status
-        flight.printStatus();
+        flight1.printStatus();
 
         // Book a seat
         for (int i = 0; i < 10; i++) {
-            flight.bookSeat();
+            flight1.bookSeat();
         }
 
         // Print status
-        flight.printStatus();
+        flight1.printStatus();
 
         // Cancel few seats
         for (int i = 0; i < 5; i++) {
-            flight.cancelBooking();
+            flight1.cancelBooking();
         }
 
         // Print status again
-        flight.printStatus();
+        flight1.printStatus();
+
+        // Flight 2
+        System.out.println("######### Flight - 2 #########");
+        flight2.printStatus();
+
+        // Flight 3
+        System.out.println("######### Flight - 3 #########");
+        flight3.printStatus();
     }
 }
